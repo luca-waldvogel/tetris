4d9d3da1b90d282f5a8e18be6c1077d3f331d542
Oguzhan Cetinkaya <cetinkaya-oguzhan@hotmail.com>
2025-06-03 20:35:14 +0200

    Zeichnungs des ersten Block

--- pygame_grundgerüst.py
+++ pygame_grundgerüst.py
@@ -1,23 +1,37 @@
 import pygame as pg
 
+ZEILEN = 20
+SPALTEN = 10
+BLOCK_GROESSE = 30
+
 # pygame ab Version 2.0 wird benötigt
 # Installation im Terminal mit
 #   --> pip install pygame (windows)
 #   --> pip3 install pygame (mac)
 #   --> sudo apt-get install python3-pygame (Linux Debian/Ubuntu/Mint)
 
+stein_pos = [SPALTEN // 2, 0] # Koodination des Blöckes mitte, ganz oben
+
 pg.init()
-größe = breite, höhe = 1920, 1080
+größe = breite, höhe = SPALTEN * BLOCK_GROESSE, ZEILEN * BLOCK_GROESSE
 fenster = pg.display.set_mode(größe)
 
 
 clock = pg.time.Clock()
+zeit = 0 # es überprüft die Zeit vom Block
 FPS = 40
-
+def zeichne_stein():
+    x, y = stein_pos
+    pg.draw.rect(
+        fenster,
+        (0, 255, 255),  # farbe des Blockes: cyan
+        (x * BLOCK_GROESSE, y * BLOCK_GROESSE, BLOCK_GROESSE, BLOCK_GROESSE)
+    )
 # Zeichenschleife mit FPS Bildern pro Sekunde
 while True:
   clock.tick(FPS)
   fenster.fill('black')
+  zeichne_stein()
 
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT or ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE: quit()
