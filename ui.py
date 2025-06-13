d3e69a67150f44430e8fa5f4a77ba10862ac5e3d
luca-waldvogel <waldvogel.luca@hotmail.com>
2025-06-13 10:33:17 +0200

    pausentext angepasst

--- ui.py
+++ ui.py
@@ -9,7 +9,7 @@ def draw_score(surface):
     surface.blit(text, (10, height + 20))
 
 def draw_pause(surface):
-    text = font.render("'P' = Pause", True, (255, 255, 255))
+    text = font.render("P = Pause", True, (255, 255, 255))
     text_rect = text.get_rect()
     x_pos = width - text_rect.width - 10  # 10 Pixel Abstand vom rechten Rand
     surface.blit(text, (x_pos, height + 20))
